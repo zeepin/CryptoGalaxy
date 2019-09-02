92469414598b7734cc34108a8b94e081a56c6d26
xyxu <37847940@qq.com>
2019-09-02 17:29:49 +0800

    v2.1.4

--- galaxy-master/src/components/HomeHandler/modules/AndroidDownload.vue
+++ galaxy-master/src/components/HomeHandler/modules/AndroidDownload.vue
@@ -5,7 +5,7 @@
             <img src="../../../assets/img/colse.png" alt="">
         </div>
       <div class="title">CryptoGalaxy</div>
-      <div class="version">2.1.3</div>
+      <div class="version">2.1.4</div>
       <div class="des">Discover &amp; Collect Your Planet</div>
       <div class="subdes">The first ever virtual universe based on blockchain</div>
      
@@ -13,12 +13,12 @@
         <p class="prompt">{{$t("banner.prompt1")}}</p>
         <a
           class="link"
-          href="http://cdn.zeepin.io/cryptogalaxy/CG_2.1.3_intl.apk"
+          href="http://cdn.zeepin.io/cryptogalaxy/CG_2.1.4_intl.apk"
         >{{$t("banner.prompt4")}}</a>
         <p class="prompt">{{$t("banner.prompt2")}}</p>
         <a
           class="link"
-          href="http://cdn.zeepin.io/cryptogalaxy/CG_2.1.3_apc.apk"
+          href="http://cdn.zeepin.io/cryptogalaxy/CG_2.1.4_apc.apk"
         >{{$t("banner.prompt4")}}</a>
       </div>
     </div>
